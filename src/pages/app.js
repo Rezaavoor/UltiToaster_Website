becf4680283d562aa2e0cf6c9467678a53171710
unknown <reza.avoor@gmail.com>
2020-01-12 19:18:41 +0100

    add background

--- src/pages/app.js
+++ src/pages/app.js
@@ -3,15 +3,19 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import { css } from '@emotion/core';
 import themes from '../themes/themes';
+import Background from '../components/Background';
+import backgroundSvg from '../../images/app-background.svg';
 
 export default function App() {
   return (
     <Layout>
       <div
         css={css`
-          height: 80vh;
+          height: 280vh;
+          position: relative;
         `}
       >
+        <Background src={backgroundSvg} />
         app
       </div>
     </Layout>
--- src/pages/get-started.js
+++ src/pages/get-started.js
@@ -2,15 +2,19 @@ import React from 'react';
 import Layout from '../components/layout';
 import { css } from '@emotion/core';
 import themes from '../themes/themes';
+import Background from '../components/Background';
+import backgroundSvg from '../../images/get-started-background.svg';
 
 export default function GetStarted() {
   return (
     <Layout>
       <div
         css={css`
-          height: 80vh;
+          height: 210vh;
+          position: relative;
         `}
       >
+        <Background width="90%" top="100px" src={backgroundSvg} />
         get started
       </div>
     </Layout>
