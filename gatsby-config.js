98641962bf1eb1f2f9ca75ca84b40d9b2a450472
unknown <reza.avoor@gmail.com>
2020-02-15 12:15:50 +0100

    add icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,16 +3,29 @@ module.exports = {
     title: 'UltiToaster Website',
     description:
       'A website about the product UltiToaster that makes your life easier than what you even can imagine',
+    siteUrl: 'https://www.ultitoaster.netlify.com',
   },
-  plugins: ['gatsby-plugin-emotion',
-   'gatsby-plugin-react-helmet',
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'UltiToaster Website',
+        short_name: 'UltiToaster',
+        start_url: 'https://www.ultitoaster.netlify.com',
+        display: 'standalone',
+        icon: 'images/Logo.png',
+      },
+    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-source-filesystem',
-      options:{
+      options: {
         name: 'images',
-        path: 'images'
-      }
-    }],
+        path: 'images',
+      },
+    },
+  ],
 };
